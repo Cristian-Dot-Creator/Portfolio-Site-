ad390f91a7cc760317724bcc75bc845c19c8fd72
Cristian-Dot-Creator <cristianmmaquez@gmail.com>
2020-07-16 01:37:28 -0400

    watch

--- src/Pages/Resume.js
+++ src/Pages/Resume.js
@@ -6,11 +6,17 @@ export default function Resume() {
     return (
         <div>
            <div className={style.resumeContainer}>
+               <div className={style.DownloadLink}>
+                   <a href="../images/Cristian - - Marquez Resume -1.jpg" download><button>
+                   Download Here!</button>
+                    </a>
+               </div>
            <div className={style.ReachContainer}>
                 <img src={resume} alt="resume" className={style.res}/>
                </div>
 
            </div>
+
 
         </div>
     )
