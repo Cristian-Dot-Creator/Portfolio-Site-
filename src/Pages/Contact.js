ca12dc98bc9a0db67cd398363c81051aa155525a
Cristian-Dot-Creator <cristianmmaquez@gmail.com>
2020-07-17 16:36:30 -0400

    more uploads

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -1,41 +1,27 @@
 import React from 'react'
 import style from '../styles/contact.module.css'
+import Git from '../images/svg/github-brands.svg';
+import LinkIn from '../images/svg/linkedin-brands.svg';
 
 export default function Contact() {
     return (
         <div className={style.contactContainer}>
-            <div className={style.container2}>
-
-               <div className={style.ReachContainer}>
-                  <div><h1>Reach Out !</h1></div>
-                    <p>Thanks for taking the time to reach out. Drop me a line below and I will personally respond as soon as possible.</p>
-                   <div><h2>Email:<a href="mailto:cristianmmaquez@gmail.com">Send Email</a></h2></div> 
-                   <div><h2>GitHub:<a href="https://github.com/Cristian-Dot-Creator">Repo</a></h2></div>
-                   <div><h2>Phone:<a>910-789-9686</a></h2></div>
-               </div>
-
             
-                <h2></h2>
-
-
-
-            </div>
-
 
             <div className={style.container}>
                         <p>Contact Me</p>   
-            <div className={style.Login}>
-                <input type="text" className={style.input} placeholder="Your Name"/>
-                <input type="text" className={style.input} placeholder="Your Email"/>
+            <div className={style.info}>
+                <div className={style.info_container}>
+                        <div className={style.info_img}></div>
+                        <div className={style.info_links}>
+                            <div className={style.info_links_Email}><h1><a href="mailto:cristianmmaquez@gmail.com">Email Me</a></h1></div>
+                            <div className={style.info_links_phone}><h1>910-789-9686</h1></div>
+                            <div className={style.info_links_linkin}><h1><a href="https://www.linkedin.com/mynetwork/">LINKEDIN</a></h1> </div>
+                            <div className={style.info_links_GitHub}><h1><a href="https://github.com/Cristian-Dot-Creator">GITHUB</a></h1></div>
+                            <div> <div className={style.logos}></div> <div className={style.logos}></div></div>
+                        </div>
+                </div>
             </div>
-            <div className={style.subject}></div>
-                <input type="text" className={style.input} placeholder="Subject"/>
-
-                <div className={style.msg}>
-            <textarea className={style.area} placeholder="Leave a Message"></textarea>
-        </div>
-        <div className={style.btn}>Send Message</div>
-
         
         </div>  
 
